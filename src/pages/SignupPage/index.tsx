a6b270271a474d90d0d1293e008a074474923009
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-03-29 18:27:51 -0300

    feat: signup request

--- src/pages/SignupPage/index.tsx
+++ src/pages/SignupPage/index.tsx
@@ -23,8 +23,15 @@ import {
 } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { signupRequest } from "../../services/apiServices";
+import { useState } from "react";
 
 export const SignupPage = () => {
+  const [name, setName] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+  const request = () => {
+    signupRequest(name, email, password)
+  }
   return (
     <FlexContainer>
       <LeftSide className="left-side">
@@ -46,6 +53,7 @@ export const SignupPage = () => {
                 className="focus:outline-blue"
                 type={"text"}
                 placeholder={"Digite o seu nome..."}
+                onChange={event => setName(event.target.value)}
               />
               <MdOutlineDriveFileRenameOutline size={28} />
             </ImputContainer>
@@ -54,6 +62,7 @@ export const SignupPage = () => {
                 className="focus:outline-blue"
                 type={"email"}
                 placeholder={"Digite seu email..."}
+                onChange={event => setEmail(event.target.value)}
               />
               <MdOutlineEmail size={28} />
             </ImputContainer>
@@ -62,10 +71,11 @@ export const SignupPage = () => {
                 className="focus:outline-blue"
                 type={"password"}
                 placeholder={"Digite uma senha..."}
+                onChange={event => setPassword(event.target.value)}
               />
               <RiLockPasswordLine size={28} />
             </ImputContainer>
-            <Button onClick={signupRequest}>Cadastre-se</Button>
+            <Button onClick={request}>Cadastre-se</Button>
           </FormContainer>
         </Container>
       </LeftSide>
--- src/services/apiServices.tsx
+++ src/services/apiServices.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export async function signupRequest() {
+export async function signupRequest(name: string, email: string, password: string) {
     try {
         const res = await axios.post('http://localhost:3333/signup', {
-            name: 'Teste api',
-            email: 'tassio.santos7@simbioseventures.com',
-            password: 'abc1234dd',
+            name: name,
+            email: email,
+            password: password,
         })
         console.log(res)
     } catch (error) {
